87204ddbeb40720b40dd537145ed54c19dfedfcc
mhShohan <mehdihasanshohan25@gmail.com>
2025-02-01 19:29:13 +0600

    cleanup repo

--- README.md
+++
@@ -1,5 +0,0 @@
-# Admin Dashboard with React
-
-### React, Recharts, Styled-components
-
-#Live View: https://admin-dashboard2321.netlify.app
--- src/components/pages/product/createProduct/CreateProduct.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import './createProduct.css';
-
-export default function CreateProduct() {
-    return <div className="createProduct">create new products</div>;
-}
